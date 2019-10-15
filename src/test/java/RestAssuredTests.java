abc9b89e37effa152867bc636dee5917df5d9150
Raul Laredo Soria <RaulLaredo@Fundacion-jala.local>
2019-10-15 17:14:40 -0400

    Added more RestAssured tests

--- src/test/java/RestAssuredTests.java
+++ src/test/java/RestAssuredTests.java
@@ -5,7 +5,7 @@ import static org.hamcrest.Matchers.*;
 
 public class RestAssuredTests {
     @Test
-    public void test_Pivotal_Tracker() {
+    public void test_Pivotal_Tracker_GetProjects() {
         String token = "bfb2dcd6dd5650981b0147da1f9301d1";
         given().
         header("X-TrackerToken",token).
@@ -15,6 +15,32 @@ public class RestAssuredTests {
         assertThat().
         statusCode(200);
     }
+
+    @Test
+    public void test_Pivotal_Tracker_GetEpics() {
+        String token = "bfb2dcd6dd5650981b0147da1f9301d1";
+        given().
+                header("X-TrackerToken",token).
+                when().
+                get("https://www.pivotaltracker.com/services/v5/epics").
+                then().
+                assertThat().
+                statusCode(200);
+    }
+
+    @Test
+    public void test_Pivotal_Tracker_GetWorkspaces() {
+        String token = "bfb2dcd6dd5650981b0147da1f9301d1";
+        given().
+                header("X-TrackerToken",token).
+                when().
+                get("https://www.pivotaltracker.com/services/v5/my/workspaces").
+                then().
+                assertThat().
+                statusCode(200);
+    }
+
+
 
 
 }
