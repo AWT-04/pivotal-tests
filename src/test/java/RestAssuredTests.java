9a5e30d3d979b489d81d1b69607f24aeab47d9f4
Raul Laredo Soria <RaulLaredo@Fundacion-jala.local>
2019-10-16 08:38:49 -0400

    Update tests

--- src/test/java/RestAssuredTests.java
+++ src/test/java/RestAssuredTests.java
@@ -1,9 +1,30 @@
 import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
 import org.junit.Test;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 public class RestAssuredTests {
+
+    @Test
+    public void test_Pivotal_Tracker_Get_Specific_Project() {
+        String token = "bfb2dcd6dd5650981b0147da1f9301d1";
+        given().
+                header("X-TrackerToken",token).
+                when().
+                get("https://www.pivotaltracker.com/services/v5/projects/2406102").
+                then().
+                assertThat().
+                statusCode(HttpStatus.SC_OK).
+                and().
+                contentType(ContentType.JSON).
+                and().
+                body("name", equalTo("AWT")).
+                and().
+                body("iteration_length", equalTo(1))
+        ;
+    }
+
     @Test
     public void test_Pivotal_Tracker_GetProjects() {
         String token = "bfb2dcd6dd5650981b0147da1f9301d1";
@@ -19,20 +40,7 @@ public class RestAssuredTests {
         ;
     }
 
-    @Test
-    public void test_Pivotal_Tracker_Get_Specific_Project() {
-        String token = "bfb2dcd6dd5650981b0147da1f9301d1";
-        given().
-                header("X-TrackerToken",token).
-                when().
-                get("https://www.pivotaltracker.com/services/v5/projects/2406102").
-                then().
-                assertThat().
-                statusCode(200).
-                and().
-                contentType(ContentType.JSON)
-        ;
-    }
+
 
     @Test
     public void test_Pivotal_Tracker_GetEpics() {
