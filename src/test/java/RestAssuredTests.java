1a0e3c4f0050e6508bff4bd6fcf317a62bc07a26
Raul Laredo Soria <RaulLaredo@Fundacion-jala.local>
2019-10-16 08:27:25 -0400

    More RestAssure tests

--- src/test/java/RestAssuredTests.java
+++ src/test/java/RestAssuredTests.java
@@ -13,19 +13,37 @@ public class RestAssuredTests {
         get("https://www.pivotaltracker.com/services/v5/projects").
         then().
         assertThat().
-        statusCode(200);
+        statusCode(200).
+        and().
+        contentType(ContentType.JSON)
+        ;
+    }
+
+    @Test
+    public void test_Pivotal_Tracker_Get_Specific_Project() {
+        String token = "bfb2dcd6dd5650981b0147da1f9301d1";
+        given().
+                header("X-TrackerToken",token).
+                when().
+                get("https://www.pivotaltracker.com/services/v5/projects/2406102").
+                then().
+                assertThat().
+                statusCode(200).
+                and().
+                contentType(ContentType.JSON)
+        ;
     }
 
     @Test
     public void test_Pivotal_Tracker_GetEpics() {
         String token = "bfb2dcd6dd5650981b0147da1f9301d1";
         given().
-                header("X-TrackerToken",token).
-                when().
-                get("https://www.pivotaltracker.com/services/v5/epics").
-                then().
-                assertThat().
-                statusCode(200);
+        header("X-TrackerToken",token).
+        when().
+        get("https://www.pivotaltracker.com/services/v5/epics").
+        then().
+        assertThat().
+        statusCode(404);
     }
 
     @Test
@@ -35,6 +53,30 @@ public class RestAssuredTests {
                 header("X-TrackerToken",token).
                 when().
                 get("https://www.pivotaltracker.com/services/v5/my/workspaces").
+                then().
+                assertThat().
+                statusCode(200);
+    }
+
+    @Test
+    public void test_Pivotal_Tracker_GetOwners() {
+        String token = "bfb2dcd6dd5650981b0147da1f9301d1";
+        given().
+                header("X-TrackerToken",token).
+                when().
+                get("https://www.pivotaltracker.com/services/v5/projects/2406102/stories/169178777/owners").
+                then().
+                assertThat().
+                statusCode(200);
+    }
+
+    @Test
+    public void test_Pivotal_Tracker_GetStories() {
+        String token = "bfb2dcd6dd5650981b0147da1f9301d1";
+        given().
+                header("X-TrackerToken",token).
+                when().
+                get("https://www.pivotaltracker.com/services/v5/projects/2406102/stories?date_format=millis&with_state=unstarted").
                 then().
                 assertThat().
                 statusCode(200);
