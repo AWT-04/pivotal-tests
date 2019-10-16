19bff729ed33559e902dfc39781a2ea49ffa379f
Andy Bazualdo Quevedo <AndyBazualdo@Fundacion-jala.local>
2019-10-16 18:24:35 -0400

    fixing sonar cloud observations 2nd try

--- src/test/java/org/fundacionjala/pivotal/Test2.java
+++ src/test/java/org/fundacionjala/pivotal/Test2.java
@@ -1,12 +1,11 @@
 package org.fundacionjala.pivotal;
 
+import io.cucumber.java.Before;
 import io.restassured.response.Response;
 import org.json.simple.parser.ParseException;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import java.io.IOException;
-
 import static io.restassured.RestAssured.given;
 
 
@@ -14,19 +13,15 @@ public class Test2 {
     private RestAssured restAssured = new RestAssured();
     private Response response;
 
-    {
-        try {
-            response = given(restAssured.getRequestSpecification())
-                        .when()
-                        .get("/projects/2406139");
-        } catch (IOException | ParseException e) {
-            System.out.println(e.getMessage());
-        }
-
+    public void setForTest() throws IOException, ParseException {
+        response = given(restAssured.getRequestSpecification())
+                .when()
+                .get("/projects/2406139");
     }
 
     @Test
-    public void GetProjectsWithSpecifications() {
+    public void GetProjectsWithSpecifications() throws IOException, ParseException {
+        setForTest();
         Assert.assertEquals(this.response.jsonPath().getString("name"), "AWT04");
     }
 }
