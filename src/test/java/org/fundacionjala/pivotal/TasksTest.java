02c980d57e828f825e7b6470266d0a0b04151aa7
ramalaso <osalamar@gmail.com>
2019-10-16 21:29:08 -0400

    Added Tasks tests

---
+++ src/test/java/org/fundacionjala/pivotal/TasksTest.java
@@ -0,0 +1,31 @@
+package org.fundacionjala.pivotal;
+
+
+import io.restassured.response.Response;
+import org.json.simple.parser.ParseException;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+import static io.restassured.RestAssured.given;
+
+public class TasksTest {
+    private RestAssured restAssured = new RestAssured();
+    private Response response;
+
+
+    public void setForTest() throws IOException, ParseException {
+        response = given(restAssured.getRequestSpecification())
+                .when()
+                .get("/projects/2406139/stories/169196012/tasks/67913037");
+    }
+
+    @Test
+    public void getTaskTypeFromStory() throws IOException, ParseException {
+        setForTest();
+        Assert.assertEquals(this.response.jsonPath().getString("kind"), "task");
+       // Assert.assertEquals(this.response.jsonPath().getString("complete"), false);
+        // Assert.assertEquals(this.response.jsonPath().getString("position"), 1);
+    }
+}
