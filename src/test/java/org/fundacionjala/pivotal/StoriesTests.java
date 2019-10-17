6d46fd0c907be773b1262bb00fab4dfe7e0b80ff
Raul Laredo Soria <RaulLaredo@Fundacion-jala.local>
2019-10-17 07:43:06 -0400

    Added copyrigth

--- src/test/java/org/fundacionjala/pivotal/StoriesTests.java
+++ src/test/java/org/fundacionjala/pivotal/StoriesTests.java
@@ -1,25 +1,43 @@
+/**
+ * Copyright (c) 2019 Jalasoft.
+ *
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jalasoft.
+ */
 package org.fundacionjala.pivotal;
 
 import io.restassured.response.Response;
 import org.json.simple.parser.ParseException;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import java.io.IOException;
-
 import static io.restassured.RestAssured.given;
 
+/**
+ * Tests Story from pivotal tracker.
+ *
+ * @author Raul Laredo
+ * @version 1.0
+ */
 public class StoriesTests {
     private RestAssured restAssured = new RestAssured();
     private Response response;
 
-
+    /*
+    Sets for get Stories
+     */
     public void setForTest() throws IOException, ParseException {
         response = given(restAssured.getRequestSpecification())
                 .when()
                 .get("/projects/2406139/stories/169196012");
     }
 
+    /*
+    Tests values get from Story
+     */
     @Test
     public void getStoryTypeFromStory() throws IOException, ParseException {
         setForTest();
@@ -28,12 +46,18 @@ public class StoriesTests {
         Assert.assertEquals(this.response.jsonPath().getString("requested_by_id"), 3294402);
     }
 
+    /*
+    Sets for post stories
+     */
     public void setForPost() throws IOException, ParseException {
         response = given(restAssured.getRequestSpecification())
                 .when()
                 .post("/projects/2406139/stories");
     }
 
+    /*
+    Tests post stories
+     */
     @Test
     public void postStoryTypeFromStory() throws IOException, ParseException {
         setForPost();
--- src/test/java/org/fundacionjala/pivotal/TasksTest.java
+++ src/test/java/org/fundacionjala/pivotal/TasksTest.java
@@ -1,26 +1,43 @@
+/**
+ * Copyright (c) 2019 Jalasoft.
+ *
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jalasoft.
+ */
 package org.fundacionjala.pivotal;
-
 
 import io.restassured.response.Response;
 import org.json.simple.parser.ParseException;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import java.io.IOException;
-
 import static io.restassured.RestAssured.given;
 
+/**
+ * Tests tasks from pivotal tracker.
+ *
+ * @author Raul Laredo
+ * @version 1.0
+ */
 public class TasksTest {
     private RestAssured restAssured = new RestAssured();
     private Response response;
 
-
+    /*
+    Sets for get Tasks
+     */
     public void setForTest() throws IOException, ParseException {
         response = given(restAssured.getRequestSpecification())
                 .when()
                 .get("/projects/2406139/stories/169196012/tasks/67913037");
     }
 
+    /*
+    Tests values get from Tasks
+     */
     @Test
     public void getTaskTypeFromStory() throws IOException, ParseException {
         setForTest();
@@ -29,12 +46,18 @@ public class TasksTest {
         // Assert.assertEquals(this.response.jsonPath().getString("position"), 1);
     }
 
+    /*
+    Sets for post tasks
+     */
     public void setForPost() throws IOException, ParseException {
         response = given(restAssured.getRequestSpecification())
                 .when()
                 .post("/projects/2406139/stories/169196012/tasks");
     }
 
+    /*
+    Tests post tasks
+     */
     @Test
     public void postTaskTypeFromStory() throws IOException, ParseException {
         setForPost();
