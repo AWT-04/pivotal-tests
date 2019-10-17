82b4a77aec0dd583bca80345440e608fb677a83e
FerHinojosa <fernando.hinojosa@live.com>
2019-10-17 17:36:20 -0400

    Fix 4 errror Travis

--- src/test/java/org/fundacionjala/pivotal/ProjectsTest.java
+++ src/test/java/org/fundacionjala/pivotal/ProjectsTest.java
@@ -7,12 +7,12 @@ import org.testng.annotations.Test;
 public class ProjectsTest {
     private ManageController restAssured = new ManageController();
     private Response response;
-    private static final String PROJECTNAME = "Lisa";
+    private static final String PROJECTNAME = "LisaII";
 
     @Test
     public void post() {
         String endPoint = "/projects";
-        String myJson = "{\"name\":\"Lisa\" }";
+        String myJson = "{\"name\":\"LisaII\" }";
         response = restAssured.setPost(endPoint, myJson);
         Assert.assertEquals(response.jsonPath().getString("name"), PROJECTNAME);
     }
