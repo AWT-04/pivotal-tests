68e6d98f1315e5d0a3354d4c805cecb5eb15509b
andyBazualdo <gato756@gmail.com>
2019-10-18 18:06:31 -0400

    update tests case

--- src/test/java/org/fundacionjala/pivotal/WorkSpaces.java
+++ src/test/java/org/fundacionjala/pivotal/WorkSpaces.java
@@ -1,17 +1,19 @@
 package org.fundacionjala.pivotal;
 
+
 import io.restassured.response.Response;
 import org.json.simple.JSONObject;
 import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class WorkSpaces {
     private Response response;
     private static final int OK = 200;
     private int id;
+    private String name;
 
-    //@Before
-    @Test
+    @BeforeTest
     public void getInitialData() {
         String path = "/my/workspaces";
         JSONObject profileContent = new JSONObject();
@@ -19,7 +21,46 @@ public class WorkSpaces {
         response = RequestManager.setPost(path, profileContent);
         response = RequestManager.setGet(path);
         id = response.jsonPath().getInt("[0].id");
-        System.out.println(id + " extract int");
+        name = response.jsonPath().getString("[0].name");
+    }
+
+    @Test
+    public void getWorkSpaces() {
+        String path = "/my/workspaces";
+        response = RequestManager.setGet(path);
+        Assert.assertEquals(response.statusCode(), OK);
+        //Assert.assertEquals(response.jsonPath().getString("name"), name);
+        System.out.println(response.jsonPath().getString("name") + "   **************************  " + name);
+    }
+
+    @Test
+    public void getWorkSpace() {
+        String path = "/my/workspaces" + "/" + id;
+        response = RequestManager.setGet(path);
+        Assert.assertEquals(response.statusCode(), OK);
+        Assert.assertEquals(response.jsonPath().getInt("id"), id);
+        System.out.println(response.jsonPath().getInt("id") + "   **************************  " + id);
+    }
+
+    @Test
+    public void putWorkSpace() {
+        String path0 = "/my/workspaces";
+        JSONObject profileContent = new JSONObject();
+        profileContent.put("name", "test 0");
+        String path = "/my/workspaces" + "/" + id;
+        response = RequestManager.setPut(path,profileContent);
+        Assert.assertEquals(response.statusCode(), OK);
+        Assert.assertEquals(response.jsonPath().getInt("id"), id);
+    }
+/*
+    @Test
+    public void createWorkSpace() {
+        String path = "/my/workspaces";
+        JSONObject profileContent = new JSONObject();
+        profileContent.put("name", "alfa");
+        response = RequestManager.setPost(path, profileContent);
+        Assert.assertEquals(response.statusCode(), OK);
+        Assert.assertEquals(response.jsonPath().getString("name"), "alfa");
     }
 
     @Test
@@ -30,47 +71,6 @@ public class WorkSpaces {
         response = RequestManager.setPost(path, profileContent);
         response = RequestManager.setGet(path);
         id = response.jsonPath().getInt("[0].id");
-        System.out.println(id + " extract int");
-    }
-
-    @Test
-    public void getWorkSpaces() {
-        String path = "/my/workspaces";
-        response = RequestManager.setGet(path);
-        Assert.assertEquals(response.statusCode(), OK);
-        Assert.assertEquals(response.jsonPath().getString("[0].name"), "A new workspace");
-    }
-
-    @Test
-    public void getWorkSpace() {
-        String path0 = "/my/workspaces";
-        response = RequestManager.setGet(path0);
-        int id = response.jsonPath().getInt("[0].id");
-        String path = "/my/workspaces" + "/" + id;
-        response = RequestManager.setGet(path);
-        Assert.assertEquals(response.statusCode(), OK);
-        Assert.assertEquals(response.jsonPath().getInt("id"), id);
-    }
-
-    @Test
-    public void putWorkSpace() {
-        String path0 = "/my/workspaces";
-        response = RequestManager.setGet(path0);
-        int id = response.jsonPath().getInt("[0].id");
-        String path = "/my/workspaces" + "/" + id;
-
-        //response = restAssured.setPut(path);
-        Assert.assertEquals(response.statusCode(), OK);
-        Assert.assertEquals(response.jsonPath().getInt("id"), id);
-    }
-
-    @Test
-    public void createWorkSpace() {
-        String path = "/my/workspaces";
-        JSONObject profileContent = new JSONObject();
-        profileContent.put("name", "alfa");
-        response = RequestManager.setPost(path, profileContent);
-        Assert.assertEquals(response.statusCode(), OK);
-        Assert.assertEquals(response.jsonPath().getString("name"), "alfa");
-    }
+        name = response.jsonPath().getString("[0].name");
+    }*/
 }
