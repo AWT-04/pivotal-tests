4287354583910e56c14224070bb90f21d0ee5ddd
FerHinojosa <fernando.hinojosa@live.com>
2019-10-18 09:37:42 -0400

    Fix 6 errror Travis

--- src/test/java/org/fundacionjala/pivotal/ProjectsTest.java
+++ src/test/java/org/fundacionjala/pivotal/ProjectsTest.java
@@ -2,19 +2,28 @@ package org.fundacionjala.pivotal;
 
 import io.restassured.response.Response;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 
 public class ProjectsTest {
     private ManageController restAssured = new ManageController();
     private Response response;
     private static final String PROJECTNAME = "LisaII";
+    private String id;
 
     @Test
     public void post() {
         String endPoint = "/projects";
         String myJson = "{\"name\":\"LisaII\" }";
         response = restAssured.setPost(endPoint, myJson);
+        id = response.jsonPath().getString("id");
         Assert.assertEquals(response.jsonPath().getString("name"), PROJECTNAME);
+    }
+
+    @AfterClass
+    public void removeProject() {
+        String endPoint = "/projects/" + id;
+        response = restAssured.setDelete(endPoint);
     }
 
     @Test
