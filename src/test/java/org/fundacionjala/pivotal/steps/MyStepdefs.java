857ec1dfcdb7191bd5c162c012950e3806f90134
Raul Laredo Soria <RaulLaredo@Fundacion-jala.local>
2019-10-21 09:55:03 -0400

    Update

--- src/test/java/org/fundacionjala/pivotal/steps/MyStepdefs.java
+++ src/test/java/org/fundacionjala/pivotal/steps/MyStepdefs.java
@@ -12,28 +12,33 @@ import java.time.LocalTime;
 
 public class MyStepdefs {
     private Response response;
-    private static final int POSITION = 3;
     private static final int VALUEOK = 200;
     private static final int VALUEOK1 = 204;
     private static final String DESCRIPTION = "description";
-    public String projectId;
-    public String storyId;
-    public String taskId;
+    private static final String TASK_DESCRIPTION = "Task description";
+    private static final String STORY_NAME = "Story name ";
+    private static final String PROJECT = "Project";
+    private static final String TASKS = "/tasks/";
+    private static final String STORIES = "/stories/";
+    private static final String PROJECTS = "/projects/";
+    private String projectId;
+    private String storyId;
+    private String taskId;
 
-    public String createProject(JSONObject body){
+    public String createProject(final JSONObject body) {
         response = RequestManager.setPost("/projects", body);
         projectId = this.response.jsonPath().getString("id");
         return projectId;
     }
 
-    public String createStory(String projectID, JSONObject bodyStory){
-        response = RequestManager.setPost("/projects/"+ projectID+"/stories", bodyStory);
+    public String createStory(final String projectID, final JSONObject bodyStory) {
+        response = RequestManager.setPost(PROJECTS + projectID + STORIES, bodyStory);
         storyId = this.response.jsonPath().getString("id");
         return storyId;
     }
 
-    public String createTask(String projectID, String storyID, JSONObject bodyTask){
-        response = RequestManager.setPost("/projects/"+ projectID+"/stories/"+storyID+"/tasks", bodyTask);
+    public String createTask(final String projectID, final String storyID, final JSONObject bodyTask) {
+        response = RequestManager.setPost(PROJECTS + projectID + STORIES + storyID + TASKS, bodyTask);
         taskId = this.response.jsonPath().getString("id");
         return taskId;
     }
@@ -43,14 +48,14 @@ public class MyStepdefs {
     @Given("A project created")
     public void aProjectCreated() {
         JSONObject projectContent = new JSONObject();
-        projectContent.put("name", "Project " + LocalTime.now() );
+        projectContent.put("name", PROJECT + LocalTime.now());
         projectId = createProject(projectContent);
     }
 
     @And("A Story created")
     public void aStoryCreated() {
         JSONObject storyContent = new JSONObject();
-        storyContent.put("name", "Story name " + LocalTime.now());
+        storyContent.put("name", STORY_NAME + LocalTime.now());
         storyId = createStory(projectId, storyContent);
     }
 
@@ -59,9 +64,9 @@ public class MyStepdefs {
 
 
         JSONObject taskContent = new JSONObject();
-        taskContent.put("description", "Task description " + LocalTime.now());
+        taskContent.put(DESCRIPTION, TASK_DESCRIPTION + LocalTime.now());
         taskId = createTask(projectId, storyId, taskContent);
-        response = RequestManager.setGet("/projects/"+projectId+"/stories/" + storyId+"/tasks/" + taskId);
+        response = RequestManager.setGet(PROJECTS + projectId + STORIES + storyId + TASKS + taskId);
     }
 
     @Then("I should see the kind as {string}")
@@ -73,14 +78,14 @@ public class MyStepdefs {
      @Given("I perform POST operation for {string}")
     public void iPerformPOSTOperationFor(final String arg0) {
          JSONObject projectContent = new JSONObject();
-         projectContent.put("name", "Project " + LocalTime.now() );
-         String projectId = createProject(projectContent);
+         projectContent.put("name", PROJECT + LocalTime.now());
+         projectId = createProject(projectContent);
          JSONObject storyContent = new JSONObject();
-         storyContent.put("name", "Story name " + LocalTime.now());
-         String storyId = createStory(projectId, storyContent);
+         storyContent.put("name", STORY_NAME + LocalTime.now());
+         String storyId1 = createStory(projectId, storyContent);
          JSONObject profileContent = new JSONObject();
-         profileContent.put(DESCRIPTION, "New Task from Rest Assured"+ LocalTime.now());
-         response = RequestManager.setPost("/projects/"+projectId+"/stories/" + storyId+"/tasks" , profileContent);
+         profileContent.put(DESCRIPTION, "New Task from Rest Assured" + LocalTime.now());
+         response = RequestManager.setPost(PROJECTS + projectId + STORIES + storyId1 + TASKS, profileContent);
     }
 
     @Then("I should see the status code as {string}")
@@ -92,17 +97,18 @@ public class MyStepdefs {
     @Given("I perform PUT operation for {string}")
     public void iPerformPUTOperationFor(final String arg0) {
         JSONObject projectContent = new JSONObject();
-        projectContent.put("name", "Project " + LocalTime.now() );
-        String projectId = createProject(projectContent);
+        projectContent.put("name", PROJECT + LocalTime.now());
+        projectId = createProject(projectContent);
         JSONObject storyContent = new JSONObject();
-        storyContent.put("name", "Story name " + LocalTime.now());
-        String storyId = createStory(projectId, storyContent);
+        storyContent.put("name", STORY_NAME + LocalTime.now());
+        String storyId2 = createStory(projectId, storyContent);
         JSONObject taskContent = new JSONObject();
-        taskContent.put("description", "Task description " + LocalTime.now());
-        String taskId = createTask(projectId, storyId, taskContent);
+        taskContent.put(DESCRIPTION, TASK_DESCRIPTION + LocalTime.now());
+        String taskId1 = createTask(projectId, storyId2, taskContent);
         JSONObject newStoryContent = new JSONObject();
-        newStoryContent.put("description", "New task description" + LocalTime.now());
-        response = RequestManager.setPut("/projects/"+ projectId +"/stories/" + storyId+"/tasks/" + taskId, newStoryContent);
+        newStoryContent.put(DESCRIPTION, "New task description" + LocalTime.now());
+        response = RequestManager.setPut(PROJECTS + projectId + STORIES
+                + storyId + TASKS + taskId1, newStoryContent);
     }
 
     @Then("I should verify the status code as {string}")
@@ -114,74 +120,21 @@ public class MyStepdefs {
     @Given("I perform DELETE operation for {string}")
     public void iPerformDELETEOperationFor(final String arg0) {
         JSONObject projectContent = new JSONObject();
-        projectContent.put("name", "Project " + LocalTime.now() );
-        String projectId = createProject(projectContent);
+        projectContent.put("name", PROJECT + LocalTime.now());
+        projectId = createProject(projectContent);
         JSONObject storyContent = new JSONObject();
-        storyContent.put("name", "Story name " + LocalTime.now());
-        String storyId = createStory(projectId, storyContent);
+        storyContent.put("name", STORY_NAME + LocalTime.now());
+        String storyId3 = createStory(projectId, storyContent);
         JSONObject taskContent = new JSONObject();
-        taskContent.put("description", "Task description " + LocalTime.now());
-        String taskId = createTask(projectId, storyId, taskContent);
+        taskContent.put(DESCRIPTION, "Task description " + LocalTime.now());
+        String taskId2 = createTask(projectId, storyId3, taskContent);
         JSONObject newStoryContent = new JSONObject();
-        newStoryContent.put("description", "New task description" + LocalTime.now());
-        response = RequestManager.setDelete("/projects/"+ projectId +"/stories/" + storyId+"/tasks/" + taskId);
+        newStoryContent.put(DESCRIPTION, "New task description" + LocalTime.now());
+        response = RequestManager.setDelete(PROJECTS + projectId + STORIES + storyId + TASKS + taskId2);
     }
 
     @Then("I should have status code as {string}")
     public void iShouldHaveStatusCodeAs(final String arg0) {
         Assert.assertEquals(this.response.statusCode(), VALUEOK1);
     }
-
-    /*Stories*/
-    @Given("I perform GET for {string}")
-    public void iPerformGETFor(final String arg0) {
-        response = RequestManager.setGet("/projects/2406102/stories/169156513");
-    }
-
-    @Then("I should see the requested_by_id as {string}")
-    public void iShouldSeeTheRequestedByIdAs(final String arg0) {
-        Assert.assertEquals(this.response.jsonPath().getString("kind"), "story");
-        Assert.assertEquals(this.response.jsonPath().getString("current_state"), "unstarted");
-    }
-
-    @Given("I perform POST  for {string}")
-    public void iPerformPOSTFor(final String arg0) {
-        JSONObject profileContent = new JSONObject();
-        profileContent.put("name", "New Story created from Rest Assured");
-        response = RequestManager.setPost("/projects/2406102/stories", profileContent);
-    }
-
-    @Then("I should see status code as {string}")
-    public void iShouldSeeStatusCodeAs(final String arg0) {
-        Assert.assertEquals(this.response.statusCode(), VALUEOK);
-    }
-
-    @Given("I perform PUT stories operation for {string}")
-    public void iPerformPUTStoriesOperationFor(final String arg0) {
-        JSONObject profileContent = new JSONObject();
-        profileContent.put("name", "New name updated from Rest Assured");
-        response = RequestManager.setPut("/projects/2406102/stories/169156513", profileContent);
-
-    }
-
-    @Then("I should verify the story status code as {string}")
-    public void iShouldVerifyTheStoryStatusCodeAs(final String arg0) {
-        Assert.assertEquals(this.response.statusCode(), VALUEOK);
-    }
-
-    @Given("I perform DELETE story operation for {string}")
-    public void iPerformDELETEStoryOperationFor(final String arg0) {
-        JSONObject profileContent = new JSONObject();
-        profileContent.put("name", "New Task delete from Rest Assured");
-        response = RequestManager.setPost("/projects/2406102/stories", profileContent);
-        String taskId = this.response.jsonPath().getString("id");
-        response = RequestManager.setDelete("/projects/2406102/stories/" + taskId);
-    }
-
-    @Then("I should have story status code as {string}")
-    public void iShouldHaveStoryStatusCodeAs(final String arg0) {
-        Assert.assertEquals(this.response.statusCode(), VALUEOK1);
-    }
-
-
 }
