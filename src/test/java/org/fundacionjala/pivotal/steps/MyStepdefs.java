90f9a8e3c20031dd3e2a4ef3d45907e18561d5b6
Raul Laredo Soria <RaulLaredo@Fundacion-jala.local>
2019-10-22 08:13:46 -0400

    Updated for passing Sonarlint and verification

--- src/test/java/org/fundacionjala/pivotal/steps/MyStepdefs.java
+++ src/test/java/org/fundacionjala/pivotal/steps/MyStepdefs.java
@@ -10,99 +10,98 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.testng.Assert;
-
-import java.time.LocalTime;
 import java.util.HashMap;
 
 public class MyStepdefs {
-    HashMap<String, Response> data = new HashMap<>();
+    private HashMap<String, Response> data = new HashMap<>();
     private Response response;
-    private String EndPoint;
-    private String ProjectId;
-    private String StoryId;
-    private String TaskId;
+    private String endPoint;
+    private String projectId;
+    private String storyId;
+    private String taskId;
+    private final int ok1 = 204;
 
-    public JSONObject getJson(String body) throws ParseException {
+    public JSONObject getJson(final String body) throws ParseException {
         JSONParser parser = new JSONParser();
         JSONObject json = (JSONObject) parser.parse(body);
         return json;
     }
 
     @Given("I perform POST operation for {string}")
-    public void iPerformPOSTOperationFor(String arg0) {
-        EndPoint = arg0;
+    public void iPerformPOSTOperationFor(final String arg0) {
+        endPoint = arg0;
     }
 
     @And("I fill the body with:")
-    public void iFillTheBodyWith(String body) throws ParseException {
-        response = RequestManager.setPost(EndPoint, getJson(body));
+    public void iFillTheBodyWith(final String body) throws ParseException {
+        response = RequestManager.setPost(endPoint, getJson(body));
     }
 
     @And("I save response as {string}")
-    public void iSaveResponseAs(String arg0) {
+    public void iSaveResponseAs(final String arg0) {
         data.put(arg0, response);
     }
 
     @And("I perform POST operation for other {string}")
-    public void iPerformPOSTOperationForOther(String arg0) {
-        ProjectId = response.jsonPath().getString("id");
-        EndPoint = arg0.replace("{ProjectId}", ProjectId);
+    public void iPerformPOSTOperationForOther(final String arg0) {
+        projectId = response.jsonPath().getString("id");
+        endPoint = arg0.replace("{ProjectId}", projectId);
     }
 
     @And("I fill the story body with:")
-    public void iFillTheStoryBodyWith(String body) throws ParseException {
-        response = RequestManager.setPost(EndPoint, getJson(body));
+    public void iFillTheStoryBodyWith(final String body) throws ParseException {
+        response = RequestManager.setPost(endPoint, getJson(body));
     }
 
     @And("I save response too as {string}")
-    public void iSaveResponseTooAs(String arg0) {
+    public void iSaveResponseTooAs(final String arg0) {
         data.put("story", response);
     }
 
     @When("I perform POST operation for a {string}")
-    public void iPerformPOSTOperationForA(String arg0) {
-        StoryId = response.jsonPath().getString("id");
-        EndPoint = arg0.replace("{SId}", StoryId);
-        EndPoint = EndPoint.replace("{ProjectId}", ProjectId);
+    public void iPerformPOSTOperationForA(final String arg0) {
+        storyId = response.jsonPath().getString("id");
+        endPoint = arg0.replace("{SId}", storyId);
+        endPoint = endPoint.replace("{ProjectId}", projectId);
     }
 
     @And("I fill the task body with:")
-    public void iFillTheTaskBodyWith(String body) throws ParseException {
-        response = RequestManager.setPost(EndPoint, getJson(body));
+    public void iFillTheTaskBodyWith(final String body) throws ParseException {
+        response = RequestManager.setPost(endPoint, getJson(body));
     }
 
     @Then("I should see the status code as {string}")
-    public void iShouldSeeTheStatusCodeAs(String arg0) {
+    public void iShouldSeeTheStatusCodeAs(final String arg0) {
         Assert.assertEquals(this.response.jsonPath().getString("kind"), "task");
     }
 
     @And("Clean environment")
     public void cleanEnvironment() {
-        RequestManager.setDelete("/projects/" + ProjectId);
+        RequestManager.setDelete("/projects/" + projectId);
     }
 
     @When("I perform PUT operation for a {string}")
-    public void iPerformPUTOperationForA(String arg0) {
-        TaskId = response.jsonPath().getString("id");
-        EndPoint = arg0.replace("{SId}", StoryId);
-        EndPoint = EndPoint.replace("{ProjectId}", ProjectId);
-        EndPoint = EndPoint.replace("{TaskId}", TaskId);
+    public void iPerformPUTOperationForA(final String arg0) {
+        taskId = response.jsonPath().getString("id");
+        endPoint = arg0.replace("{SId}", storyId);
+        endPoint = endPoint.replace("{ProjectId}", projectId);
+        endPoint = endPoint.replace("{TaskId}", taskId);
 
     }
 
     @And("I fill the task body with new name:")
-    public void iFillTheTaskBodyWithNewName(String body) throws ParseException {
-        response = RequestManager.setPut(EndPoint, getJson(body));
+    public void iFillTheTaskBodyWithNewName(final String body) throws ParseException {
+        response = RequestManager.setPut(endPoint, getJson(body));
     }
 
     @When("Delete project for {string}")
-    public void deleteProjectFor(String arg0) {
-        ProjectId = data.get("Project").jsonPath().getString("id");
-        response = RequestManager.setDelete(arg0 + ProjectId);
+    public void deleteProjectFor(final String arg0) {
+        projectId = data.get("Project").jsonPath().getString("id");
+        response = RequestManager.setDelete(arg0 + projectId);
     }
 
     @Then("I should see the status code {string}")
-    public void iShouldSeeTheStatusCode(String arg0) {
-        Assert.assertEquals(this.response.statusCode(), 204);
+    public void iShouldSeeTheStatusCode(final String arg0) {
+        Assert.assertEquals(this.response.statusCode(), ok1);
     }
 }
