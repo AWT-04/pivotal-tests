4d7785a11723eddb201c19823b83a559cfba967f
Raul Laredo Soria <RaulLaredo@Fundacion-jala.local>
2019-10-24 08:06:10 -0400

    Update

--- src/test/java/org/fundacionjala/pivotal/steps/ResponseStepDefs.java
+++ src/test/java/org/fundacionjala/pivotal/steps/ResponseStepDefs.java
@@ -1,18 +1,4 @@
 package org.fundacionjala.pivotal.steps;
 
-import io.restassured.response.Response;
-import org.fundacionjala.pivotal.ScenarioContext;
-
 public class ResponseStepDefs {
-    private ScenarioContext context;
-    private Response response;
-    private String endPoint;
-    private String projectId;
-    private String storyId;
-    private static final int OK1 = 204;
-    private static final String PROJECT_ID = "{ProjectId}";
-
-    public ResponseStepDefs(final ScenarioContext context) {
-        this.context = context;
-    }
 }
