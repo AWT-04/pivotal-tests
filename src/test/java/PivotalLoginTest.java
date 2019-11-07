32ddf1a3e9b30483e7b5d4f095bcbe7a198dfe62
ramalaso <osalamar@gmail.com>
2019-11-07 14:46:38 -0400

    Update test

--- src/test/java/PivotalLoginTest.java
+++ src/test/java/PivotalLoginTest.java
@@ -2,42 +2,43 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import java.time.LocalDateTime;
-import java.util.Date;
-import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 public class PivotalLoginTest {
 
     @Test
     public void login() throws InterruptedException {
+        final String title = LocalDateTime.now().toString();
         WebDriverManager.chromedriver().setup();
         WebDriver browser = new ChromeDriver();
-        final String title = LocalDateTime.now().toString();
+        browser.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 
         browser.get("https://www.pivotaltracker.com/signin");
-
-
         browser.findElement(By.cssSelector("#credentials_username")).sendKeys("raullaredo");
-
         browser.findElement(By.cssSelector(".app_signin_action_button")).click();
-
         browser.findElement(By.cssSelector("#credentials_password")).sendKeys("r4514812L*");
-
         browser.findElement(By.cssSelector(".app_signin_action_button")).click();
         browser.findElement(By.cssSelector("#create-project-button")).click();
-
         browser.findElement(By.cssSelector(".tc-form__input")).sendKeys(title);
         browser.findElement(By.cssSelector(".tc-account-selector__header")).click();
         browser.findElement(By.xpath("//*[@id=\"modal_area\"]/div/div[2]/div/form/div/div/fieldset/div/div[2]/div[1]/ul/li/span[1]/div")).click();
-
         browser.findElement(By.cssSelector(".pvXpn__Button--positive")).click();
-        browser.wait(5000);
-        Assert.assertFalse(browser.getTitle().contains(title));
+        (new WebDriverWait(browser, 30)).until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(".scrimVisible")));
+        Assert.assertTrue(browser.getTitle().contains(title));
+        browser.quit();
+    }
 
+    public static void ExplicitWait(WebDriver driver, String css){
+        (new WebDriverWait(driver, 10)).until(ExpectedConditions.elementToBeClickable(By.cssSelector(css)));
+    }
 
-
+    public static void ImplicitWait(WebDriver driver){
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
     }
 }
+
